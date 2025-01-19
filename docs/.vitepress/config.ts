37de35165eb4db1afc53a6dfd98d34799131925c
diarboy <bigdickdumb@gmail.com>
2025-01-19 19:18:41 +0700

    latex

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitepress';
+import mathjax3 from 'markdown-it-mathjax3';
 
 export default defineConfig({
   lang: 'en-US',
@@ -9,8 +10,13 @@ export default defineConfig({
   head: [
     ['link', { rel: 'icon', type: 'image/x-icon', href: '/allbibek/favicon.svg' }],
     ['link', { rel: 'stylesheet', href: '/allbibek/custom.css' }],
-    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css' }],
     ],
+  
+  markdown: {
+    config: (md) => {
+      md.use(mathjax3); // Integrasikan MathJax untuk mendukung persamaan matematika
+    },
+  },
 
   themeConfig: {
     logo: '/favicon.svg',
