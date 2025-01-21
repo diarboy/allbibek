bbe986ab03925dbf7bfee730b604e693e4550875
diarboy <bigdickdumb@gmail.com>
2025-01-21 18:31:50 +0700

    datedisplay

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import FAQ from '../../components/FAQ.vue'
+import DateDisplay from '../../components/DateDisplay.vue'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.component('FAQ', FAQ)
+    app.component('DateDisplay', DateDisplay)
   }
 }
